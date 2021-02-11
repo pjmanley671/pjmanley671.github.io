295ae9e3c40b3d1e0177c20e4856683b8221f070
Paul Manley <35860068+pjmanley671@users.noreply.github.com>
2021-02-10 23:54:13 -0600

    Modularize Chart code
    Seperated code from TableCommit to turn into resizable functions to allow for OnResize behaviors to be accounted for.

--- site-behavior.js
+++ site-behavior.js
@@ -1,43 +1,70 @@
-var thisDate = new Date();
+var pointCommits = new Uint32Array(12);
+
+function ResetPointPositions()
+{
+	var svg = document.getElementById("chart");
+	var points = svg.children;
+	var xoffset = 30;
+	
+	for(p = 0; p < points.length; p++)
+	{
+		points[p].setAttribute("cx", String((svg.clientWidth / points.length) * p + (svg.clientWidth / xoffset)));
+		points[p].setAttribute("cy", String(svg.clientHeight));
+	}
+}
+
+function SetPointPositions()
+{
+	var svg = document.getElementById("chart");
+	var points = svg.children;
+	var goal = 10;
+
+	for(j = 0; j < points.length; j++)
+	{
+		var height = svg.clientHeight - (pointCommits[j] * (svg.clientHeight / goal));
+		if(height < 0) height = 0;
+		
+		points[j].setAttribute("cy", String(height));
+	}
+}
+
+function OnResize()
+{
+	ResetPointPositions();
+	SetPointPositions();
+}
 
 // https://stackoverflow.com/questions/10087819/convert-date-to-another-timezone-in-javascript
 function convertTZ(date, tzString) {
-    return new Date((typeof date === "string" ? new Date(date) : date).toLocaleString("en-US", {timeZone: tzString}));   
+    return new Date((typeof date === "string" ? new Date(date) : date).toLocaleString("en-US", {timeZone: tzString}));
 }
 
-function TableCommit(commits = {}, repo)
-{
-	var i = 0, xoffset = 30, months = 12, goal = 10;
+function TableCommit(commits = {}, repo, thisDate) {
+	var i = 0;
 	var svg = document.getElementById("chart");
-	var points = svg.children;
-	for(p = 0; p < points.length; p++)
-	{ // Resets the position of points to fit into the baseline positions of the chart.
-		points[p].setAttribute("cx", String((svg.clientWidth / months) * p + (svg.clientWidth / xoffset)));
-		points[p].setAttribute("cy", String(svg.clientHeight));
-	}
+
+	ResetPointPositions();
 
 	/* Iterates throught the commits and filters based off if they were commited this year. Then adjusts the point on the chart accordingly. */
-	for(cmt in commits)
-	{
+	for(cmt in commits)	{
 		var centralTime = convertTZ(commits[cmt].commit.author.date, 'America/Chicago');
 		if(centralTime.getUTCFullYear() == thisDate.getFullYear())
 		{
-			var point = points[centralTime.getMonth()];
-			var cy = parseInt(point.getAttribute("cy"));
-			if(cy > (0 + (svg.clientHeight / goal)))
-				point.setAttribute("cy", String(cy - (svg.clientHeight / goal)));
+			pointCommits[centralTime.getMonth()] += 1;
 			i++;
 		}
 	}
+	
+	SetPointPositions();
+
 	/* Commits the incrementation as the total number of commits for the year into the '# of Commits' column in the table. */
-	let childs = document.getElementById("table-details").children[0].children;
-	let repoChild = childs[repo].children;
-	repoChild[2].innerHTML = i;
+	let childs = document.getElementById("table-details").children[0].children[repo].children;
+	childs[2].innerHTML = i;
 }
 
-function fillTable(pUser = {})
-{
+function FillTable(pUser = {}) {
 	var i = 0; // Keeps track of each repo updated this year
+	var thisDate = new Date();
 	for(let repo in pUser)
 	{
 		var centralTime = convertTZ(pUser[repo].pushed_at, 'America/Chicago');
@@ -48,7 +75,8 @@ function fillTable(pUser = {})
 			let tableColumns = [];
 			var tableRow = document.createElement("tr");
 			
-			for(j = 0; j < 3; j++) tableColumns.push(document.createElement("td"));
+			for(j = 0; j < 3; j++) 
+				tableColumns.push(document.createElement("td"));
 			
 			/* Stores the link to the repository and the repository name */
 			var a = document.createElement("a");
@@ -61,7 +89,8 @@ function fillTable(pUser = {})
 			tableColumns[1].appendChild(a);
 			tableColumns[2].innerHTML = " ";
 
-			for(j = 0; j < tableColumns.length; j++) tableRow.appendChild(tableColumns[j]);
+			for(j = 0; j < tableColumns.length; j++) 
+				tableRow.appendChild(tableColumns[j]);
 
 			/* Sends the table information to the webpage table */
 			document.getElementById("table-details").children[0].appendChild(tableRow);
@@ -69,19 +98,17 @@ function fillTable(pUser = {})
 			/* Gets the commit details and sends information to the table. */
 			fetch(pUser[repo].commits_url.slice(0, pUser[repo].commits_url.length - 6))
 				.then(response => {return response.json()})
-				.then(data => TableCommit(data, i));
+				.then(data => TableCommit(data, i, thisDate));
 		}
 	}
 }
 
-function GetRepos(url='')
-{
+function GetRepos(url='') {
 	fetch(url)
 	.then(response => {return response.json()})
-	.then(data => fillTable(data));
+	.then(data => FillTable(data));
 }
-function PageLoad()
-{
+function PageLoad() {
 	switch(document.getElementById("page-name").innerHTML)
 	{
 		case "Paul Manley - Portfolio":
