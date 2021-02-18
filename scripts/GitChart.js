bdfa6880b581d230626dc55aeaa36cf35ff91ce8
Paul Manley <35860068+pjmanley671@users.noreply.github.com>
2021-02-18 15:53:00 -0600

    Setting up json files
    Json files setup for cv testing.

--- scripts/GitChart.js
+++ scripts/GitChart.js
@@ -12,7 +12,8 @@ function ResetMonths(){
 		l_XOffset 	= l_SVG.clientWidth / 50;
 
 	for(let i = 0; i < l_Months.children.length; i++)
-		l_Months.children[i].setAttribute("x", String(i * l_SVG.clientWidth / l_Months.children.length + l_XOffset));
+		l_Months.children[i].setAttribute("x", String(
+			i * l_SVG.clientWidth / l_Months.children.length + l_XOffset));
 }
 
 function ResetPoints(p_SVG){
@@ -20,8 +21,9 @@ function ResetPoints(p_SVG){
 		l_XOffset	= p_SVG.clientWidth / 30;
 	
 	for(let p = 0; p < l_Points.length; p++){
-			l_Points[p].setAttribute("cx", String(p * p_SVG.clientWidth / l_Points.length + l_XOffset));
-			
+			l_Points[p].setAttribute("cx", String(
+				p * p_SVG.clientWidth / l_Points.length + l_XOffset));
+
 			l_Points[p].setAttribute("cy", String(p_SVG.clientHeight));
 	}
 }
@@ -33,8 +35,6 @@ function SetPointPositions(p_SVG, p_Goal){
  		let height = p_SVG.clientHeight - 
 			(g_Commits[i] - Math.floor(g_Commits[i] / p_Goal) * p_Goal) 
 			* p_SVG.clientHeight / p_Goal;
-
-		if(height < 0) height = 0;
 		
 		l_Points[i].setAttribute("cy", String(height));
 	}
@@ -45,31 +45,31 @@ function ResetLines(p_SVG, p_Goal){
 		l_Lines 		= document.getElementById("LineGroup").children;
 
 	for(let i = 0; i < l_Lines.length; i++)
-	switch(l_Lines[i].id){
-		case "x-axis":
-			l_Lines[i].setAttribute("y2", String(p_SVG.clientHeight));
-			l_Lines[i].setAttribute("x1", String(p_SVG.clientLeft + 1));
-			l_Lines[i].setAttribute("x2", String(p_SVG.clientLeft + 1));
-			break;
-		case "y-axis": 
-			l_Lines[i].setAttribute("x2", String(p_SVG.clientWidth));
-			l_Lines[i].setAttribute("y1", String(p_SVG.clientHeight - 1));
-			l_Lines[i].setAttribute("y2", String(p_SVG.clientHeight - 1));
-			break;
-		default: 
-			l_LineNumber++;
-			l_Lines[i].setAttribute("x2", String(p_SVG.clientWidth));
-			l_Lines[i].setAttribute("y1", String(
-				p_SVG.clientHeight - l_LineNumber * p_SVG.clientHeight / p_Goal));
-			l_Lines[i].setAttribute("y2", String(
-				p_SVG.clientHeight - l_LineNumber * p_SVG.clientHeight / p_Goal));
-			break;
-	}
+		switch(l_Lines[i].id){
+			case "x-axis":
+				l_Lines[i].setAttribute("y2", String(p_SVG.clientHeight));
+				l_Lines[i].setAttribute("x1", String(p_SVG.clientLeft + 1));
+				l_Lines[i].setAttribute("x2", String(p_SVG.clientLeft + 1));
+				break;
+			case "y-axis": 
+				l_Lines[i].setAttribute("x2", String(p_SVG.clientWidth));
+				l_Lines[i].setAttribute("y1", String(p_SVG.clientHeight - 1));
+				l_Lines[i].setAttribute("y2", String(p_SVG.clientHeight - 1));
+				break;
+			default: 
+				l_LineNumber++;
+				l_Lines[i].setAttribute("x2", String(p_SVG.clientWidth));
+				l_Lines[i].setAttribute("y1", String(
+					p_SVG.clientHeight - l_LineNumber * p_SVG.clientHeight / p_Goal));
+				l_Lines[i].setAttribute("y2", String(
+					p_SVG.clientHeight - l_LineNumber * p_SVG.clientHeight / p_Goal));
+				break;
+		}
 }
 
 export function DrawChart(){
-	var l_SVG 		= document.getElementById("chart"), 
-		l_Goal 		= 10,
+	var l_SVG 			= document.getElementById("chart"), 
+		l_Goal 			= 10,
 		l_GithubMark 	= l_SVG.children[0];
 
 		ResetLines(l_SVG, l_Goal);
@@ -87,10 +87,11 @@ function UpdateTable(p_RepoName, p_PushedDate, p_CommitTotal, p_RepoURL){
 	if(p_RepoName    == null || undefined || '') return;
 	if(p_CommitTotal == null || undefined || '') return;
 	if(p_PushedDate  == null || undefined || '') return;
+	if(p_RepoURL	 == null || undefined || '') return;
 
-	const COLUMNS = 3;
-	var l_Table		 = document.getElementById("table-details");
-	var l_TableRow 	 = document.createElement("tr");
+	const COLUMNS 	= 3;
+	var l_Table		= document.getElementById("table-details");
+	var l_TableRow 	= document.createElement("tr");
 
 	var l_button 	 	= document.createElement("button");
 	l_button.innerHTML 	= p_RepoName;
@@ -99,7 +100,7 @@ function UpdateTable(p_RepoName, p_PushedDate, p_CommitTotal, p_RepoURL){
 		window.open(event.target.value, "_self");
 	})
 
-	for(var i = 0; i < COLUMNS; i++){
+	for(let i = 0; i < COLUMNS; i++){
 		let column = document.createElement("td");
 		switch(i){
 			case 0: column.appendChild(l_button); break;
@@ -120,8 +121,7 @@ export async function GetAndHandleRepos(url=''){
 
 	l_User.forEach(repo => {
 		let repoCentralTime = convertTZ(repo.pushed_at, 'America/Chicago');
-		if(repoCentralTime.getFullYear() === l_Date.getFullYear()) 
-			l_RecentPushedRepos.push(repo);
+		if(repoCentralTime.getFullYear() === l_Date.getFullYear()) l_RecentPushedRepos.push(repo);
 	});
 
 	l_RecentPushedRepos.forEach(async function(repo){
@@ -129,11 +129,11 @@ export async function GetAndHandleRepos(url=''){
 		let commits = await (await fetch(repo.commits_url.slice(0, repo.commits_url.length - 6))).json();
 		let recentCommits = [];
 
-		commits.forEach(commit => {
-			let commitCentralTime = convertTZ(commit.commit.author.date, 'America/Chicago');
+		commits.forEach(cmt => {
+			let commitCentralTime = convertTZ(cmt.commit.author.date, 'America/Chicago');
 
 			if(commitCentralTime.getFullYear() == l_Date.getFullYear()) {
-				recentCommits.push(commit);
+				recentCommits.push(cmt);
 				g_Commits[commitCentralTime.getMonth()]++;
 			}
 		})
