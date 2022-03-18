a10071e669b1ea9c88e4c950a16d9aae64bbfbdf
Paul Manley <pjmanley671@gmail.com>
2022-03-17 19:08:04 -0500

    Renaming and added 'advanced function' for determining text length. Added condition where if the text length is less than half the 'window space' then stop the animation for the footer. Started using backticks.

--- scripts/Utils.js
+++ scripts/Utils.js
@@ -4,37 +4,48 @@ export function convertTZ(date, tzString){
 		new Date(date) : date).toLocaleString("en-US", {timeZone: tzString}));
 }
 
-export function AdjustAnimationSpeedByText(pId){
-	let docItem = document.getElementById(pId);
-	if(docItem == null) return;
-	let elemStyle = window.getComputedStyle(docItem, null);
-	if(elemStyle == null) return;
-
-	if(elemStyle.getPropertyValue("animation-duration") == null || elemStyle.getPropertyValue("-webkit-animation-duration") == null)
-		return;
-
-	let numCharacters = docItem.innerHTML.length;
-	
-	if(numCharacters <= 0){
-		docItem.style["animation-duration"] = 0 + "ms";
-		docItem.style["-webkit-animation-duration"] = 0 + "ms";
-		return;
-	}
-	
-	let fontSize = parseFloat(elemStyle.getPropertyValue("font-size"));
-	let width = (window.innerWidth > 0)? window.innerWidth: screen.width;
-	let Offset = fontSize * numCharacters;
-	let newSpeed = width + Offset;
-
-	docItem.style["animation-duration"] = newSpeed / 90 + "s";
-	docItem.style["-webkit-animation-duration"] = newSpeed / 90 + "s";
+function displayTextWidth(text, font){
+	// https://www.w3docs.com/snippets/javascript/how-to-calculate-text-width-with-javascript.html
+  let canvas = displayTextWidth.canvas || (displayTextWidth.canvas = document.createElement("canvas"));
+  let context = canvas.getContext("2d");
+  context.font = font;
+  let metrics = context.measureText(text);
+  return metrics.width;
 }
 
-export function GenerateLinkButton(p_Text, p_Url){
-  var l_btn = document.createElement("button");
-	l_btn.innerHTML = p_Text;
-  l_btn.value = p_Url;
-  return l_btn;
+export function AdjustAnimationSpeedByText(pId){
+	let document_object = document.getElementById(pId);
+	if(document_object == null) return;
+	let element_style = window.getComputedStyle(document_object, null);
+	if(element_style == null) return;
+
+	if(element_style.getPropertyValue("animation-duration") == null || element_style.getPropertyValue("-webkit-animation-duration") == null)
+		return;
+
+	let object_text_width = displayTextWidth(document_object.innerHTML, element_style);
+	let display_width = parseInt(element_style.width);
+	let newSpeed = display_width + object_text_width;
+
+	console.log(
+		`Content Size = ${object_text_width}px
+		Screen Space = ${display_width}px`
+	)
+
+	if(object_text_width < (display_width / 2)){
+		document_object.style["animation-duration"] = 0 + "ms";
+		document_object.style["-webkit-animation-duration"] = 0 + "ms";
+		return;
+	}
+
+	document_object.style["animation-duration"] = newSpeed / 90 + "s";
+	document_object.style["-webkit-animation-duration"] = newSpeed / 90 + "s";
+}
+
+export function GenerateLinkButton(pText, pUrl){
+  let document_button = document.createElement("button");
+	document_button.innerHTML = pText;
+  document_button.value = pUrl;
+  return document_button;
 }
 
 export function OpenPage(elmnt, color){
@@ -44,14 +55,13 @@ export function OpenPage(elmnt, color){
 	tablinks = document.getElementsByClassName("Navbar-link");
 
 	for (i = 0; i < tabcontent.length; i++)
-		tabcontent[i].style.display = "none"; // collapse all the pages content.
+		tabcontent[i].style.display = "none"; // collapse all pages.
 
 	for (i = 0; i < tablinks.length; i++)
 		tablinks[i].style.backgroundColor = "#555"; // reset all the navbar buttons colors.
 
-	// Set the display information for the active page.
 	let activePage = document.getElementById(elmnt.innerHTML);
-	let width = (window.innerWidth > 0)? window.innerWidth: screen.width;
-	activePage.style.display = (width > 800)? "flex" : "inline-block";
+	let display_width = (window.innerWidth > 0)? window.innerWidth: screen.width;
+	activePage.style.display = (display_width > 800)? "flex" : "inline-block";
 	elmnt.style.backgroundColor = color;
 }
--- site-behavior.js
+++ site-behavior.js
@@ -4,8 +4,10 @@ import Config from './Data/General.js'
 
 var activePage;
 
-async function GetAndHandleRepos(url=''){
-	var user_repos, commit_count, repos_recent, date_year_current;
+async function GetAndHandleRepos(url=''){ // Updates only on PageLoad
+	if(url === null || url === undefined || url === '') return;
+	let user_repos, repos_recent;
+	var commit_count, date_year_current; // using in nested async function.
 	repos_recent = [];
 	commit_count = new Uint32Array(document.getElementById("CircleGroup").children.length);
 
@@ -13,19 +15,19 @@ async function GetAndHandleRepos(url=''){
 	user_repos = await (await fetch(url)).json();
 	date_year_current = (new Date()).getFullYear();
 
-	user_repos.forEach(repo =>{
+	user_repos.forEach(repo=>{
 		let repo_pushed_year;
 		repo_pushed_year = (Utils.convertTZ(repo.pushed_at, TIMEZONE)).getFullYear();
 		if(repo_pushed_year === date_year_current) repos_recent.push(repo);
 	})
 
 	repos_recent.forEach(async function(repo){
-		let repo_pushed_date, commits, commits_count, commit_url, repo_html_button;
+		let repo_pushed_date, commits, repo_commits_count, commit_url, repo_html_button, commits_page_number;
 		
-		commits_count = 0;
+		repo_commits_count = 0; commits_page_number = 1;
 		repo_pushed_date = Utils.convertTZ(repo.pushed_at, TIMEZONE);
 		commit_url = repo.commits_url.slice(0, repo.commits_url.length - 6);
-		commit_url += "?per_page=100&page=1";
+		commit_url += `?per_page=100&page=${commits_page_number}`;
 
 		let fetch_response = await(fetch(commit_url));
 		if(fetch_response.ok){
@@ -33,28 +35,28 @@ async function GetAndHandleRepos(url=''){
 			let doc_url = "";
 			doc_url = document.URL;
 
-			// checks position to be 8 because the site name should be https:// and then the github repo name.
-			if(doc_url.search(repo.name) === 8 || repo.name == "pjmanley671.github.io"){ // Uses my specific site to check for live server testing.
+			// checks position to be 8 because the site name should be https:// and then the github site name.
+			if(doc_url.search(repo.name) === 8 || repo.name === "pjmanley671.github.io"){ // Uses my specific site to check for live server testing.
 				let site_update_latest = document.getElementById("LastSiteUpdate");
 				site_update_latest.innerHTML += commits[0].commit.message;
 				Utils.AdjustAnimationSpeedByText("LastSiteUpdate");
 			}
 
-			commits.forEach(cmt =>{
+			commits.forEach(cmt=>{
 				let commit_date;
 				commit_date = Utils.convertTZ(cmt.commit.committer.date, TIMEZONE);
-				if(commit_date.getFullYear() == date_year_current){
-					commits_count++;
+				if(commit_date.getFullYear() === date_year_current){
+					repo_commits_count++;
 					commit_count[commit_date.getMonth()] += 1;
 				}
 			})
 			repo_html_button = Utils.GenerateLinkButton(repo.name, repo.html_url);
 			repo_html_button.addEventListener("click", event=>{
 				let exit_confirm = false;
-				exit_confirm = confirm("Page will exit to Github repository: " + event.target.innerHTML + ". Continue?");
+				exit_confirm = confirm("Page will exit to Github repository: "+ event.target.innerHTML +". Continue?");
 				if(exit_confirm) window.open(event.target.value, "_self");
 			});
-			UpdateTable(repo_html_button, repo_pushed_date.toDateString(), commits_count);
+			UpdateTable(repo_html_button, repo_pushed_date.toDateString(), repo_commits_count);
 			DrawChart();
 		}
 	})
@@ -66,14 +68,14 @@ function GenerateHeaderButtons(){
 	navbar = document.getElementById("navbar");
 	dropdown_content = document.getElementById("dropdown-content");
 
-	Config.Links.forEach(navLink =>{
+	Config.Links.forEach(navLink=>{
 		let nav_button;
 		nav_button = Utils.GenerateLinkButton(navLink.Name, navLink.Link);
 		nav_button.className = navLink.Confirmation.message_format;
 
 		switch(navLink.Confirmation.message_format){
 			case "Navbar-link":
-				nav_button.addEventListener("click", event =>{
+				nav_button.addEventListener("click", event=>{
 					Utils.OpenPage(event.target, "black");
 					activePage = event.target.innerHTML;
 					Resize();
@@ -81,7 +83,7 @@ function GenerateHeaderButtons(){
 				navbar.appendChild(nav_button);
 				break;
 			case "Perlenspiel":
-				nav_button.addEventListener("click", event =>{
+				nav_button.addEventListener("click", event=>{
 					let exit_confirm = false;
 					exit_confirm = confirm("Page will transfer to perlesnpiel project: "+ event.target.innerHTML +". Continue?");
 					if(exit_confirm) window.open(event.target.value, "_self");
