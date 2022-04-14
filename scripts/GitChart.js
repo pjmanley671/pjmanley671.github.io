8c1621f65c0e4489148b97c8e7751771357a7489
Paul Manley <pjmanley671@gmail.com>
2022-04-14 02:00:51 -0500

    Optimized GitChart.js functions. Reversed for loops due to several posts saying they are slightly better. Moved Dates switch case into seperate function that returns the date as a value and then converts to string for simplicity. Chose not to use Object Literal for this case since it reportedly much slower.

--- scripts/GitChart.js
+++ scripts/GitChart.js
@@ -2,6 +2,7 @@ const chart_months = document.getElementById("CircleGroup");
 const chart_svg = document.getElementById("chart-x-definitions");
 const chart = document.getElementById("chart");
 const GOAL = 10;
+const githubmark = chart.children[0];
 
 var g_Commits = new Uint32Array(chart_months.length);
 
@@ -13,8 +14,8 @@ function ResetMonths(){
 	xOffset	= width / 50;
 	xDist = width / months.children.length;
 
-	for(let i = 0; i < months.children.length; i++)
-		months.children[i].setAttribute("x", String(i * xDist + xOffset));
+	for(let i = months.children.length; i; i--) // used reverse loop since method can be called frequently on Resize
+		months.children[i-1].setAttribute("x", String((i-1) * xDist + xOffset));
 }
 
 function ScalePointsX(){
@@ -23,21 +24,20 @@ function ScalePointsX(){
 	points = chart_months.children;
 	xOffset= chart.clientWidth / 30
 	
-	for(let p = 0; p < points.length; p++)
-		points[p].setAttribute("cx", String(p * chart.clientWidth / points.length + xOffset));
+	for(let p = points.length; p; p--) // used reverse loop since method can be called frequently on Resize
+		points[p-1].setAttribute("cx", String( (p-1) * chart.clientWidth / points.length + xOffset));
 }
 
 function SetPointY(){
 	let points, yDist
 
 	points = chart_months.children;
-	yDist = chart.clientHeight / GOAL
+	yDist = chart.clientHeight / GOAL;
 
-	for(let i = 0; i < points.length; i++){
-		const height = chart.clientHeight - g_Commits[i] * yDist;
-		const isNotGoalHeight = Number(g_Commits[i] < GOAL);
-
-		points[i].setAttribute("cy", String(isNotGoalHeight * height));
+	for(let i = points.length; i; i--){ // used reverse loop since method can be called frequently on Resize
+		const height = chart.clientHeight - g_Commits[i-1] * yDist;
+		const isNotGoalHeight = Number(g_Commits[i - 1] < GOAL);
+		points[i-1].setAttribute("cy", String(isNotGoalHeight * height));
 	}
 }
 
@@ -65,7 +65,7 @@ const LinesMap = {
 };
 
 function ResetLines(){
-	var lineNumber, lines;
+	let lineNumber, lines;
 
 	lineNumber = 0;
 	lines = chart.children[2].children;
@@ -73,35 +73,39 @@ function ResetLines(){
 }
 
 export function DrawChart(){
-	var githubmark;
-
 	ResetLines();
 	ResetMonths();
 	ScalePointsX();
 	SetPointY();
 
-	githubmark = chart.children[0];
 	githubmark.setAttribute( "x",
 		String(0.5 * chart.clientWidth - 0.75 
 			* parseInt(githubmark.getAttribute("width"))));
 }
 
+const DateValue = (pDate) => {
+	let value = 0;
+	switch(pDate){
+		case "Jan": value = 1; break;
+		case "Feb": value = 2; break;
+		case "Mar": value = 3; break;
+		case "Apr": value = 4; break;
+		case "May": value = 5; break;
+		case "Jun": value = 6; break;
+		case "Jul": value = 7; break;
+		case "Aug": value = 8; break;
+		case "Sep": value = 9; break;
+		case "Oct": value = 10; break;
+		case "Nov": value = 11; break;
+		case "Dec": value = 12; break;
+		default: value = -100; break;
+	}
+	return value;
+}
+
 export function ReformatStringDate(pDate){
 	let lDate;
-	switch(pDate.substring(0, 3)){
-		case "Jan": lDate = "1"; break;
-		case "Feb": lDate = "2"; break;
-		case "Mar": lDate = "3"; break;
-		case "Apr": lDate = "4"; break;
-		case "May": lDate = "5"; break;
-		case "Jun": lDate = "6"; break;
-		case "Jul": lDate = "7"; break;
-		case "Aug": lDate = "8"; break;
-		case "Sep": lDate = "9"; break;
-		case "Oct": lDate = "10"; break;
-		case "Nov": lDate = "11"; break;
-		case "Dec": lDate = "12"; break;
-	}
+	lDate = "" + DateValue(pDate.substring(0, 3));
 	return lDate.concat(pDate.substring(3, pDate.length));
 }
 
